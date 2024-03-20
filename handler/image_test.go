72d0b315660172e861ea82171dd9815d6f696640
Abyan Rizqo <rizqo1412@gmail.com>
2024-03-20 07:13:12 +0700

    test: refactor handler test

--- handler/image_test.go
+++ handler/image_test.go
@@ -2,6 +2,7 @@ package handler
 
 import (
 	"bytes"
+	"fmt"
 	"io"
 	"mime/multipart"
 	"net/http"
@@ -14,169 +15,93 @@ import (
 )
 
 func Test_imageHandler_ProcessImage(t *testing.T) {
-
-	t.Run("success process image", func(t *testing.T) {
-		// generate body
-		bodyWithFile := new(bytes.Buffer)
-		mwWithFile := multipart.NewWriter(bodyWithFile)
-
-		file, err := os.Open("./image/flower.png")
-		if err != nil {
-			t.Fatal(err)
-		}
-
-		ws, err := mwWithFile.CreateFormFile("file", "flower.png")
-		if err != nil {
-			t.Fatal(err)
-		}
-
-		if _, err := io.Copy(ws, file); err != nil {
-			t.Fatal(err)
-		}
-
-		resizeHeight, _ := mwWithFile.CreateFormField("resizeHeight")
-		resizeHeight.Write([]byte("50"))
-
-		resizeWidth, _ := mwWithFile.CreateFormField("resizeWidth")
-		resizeWidth.Write([]byte("50"))
-
-		mwWithFile.Close()
-		// end generate body
-
-		w := httptest.NewRecorder()
-		c, _ := gin.CreateTestContext(w)
-		reqWithFile, err := http.NewRequest(http.MethodPost, "/", bodyWithFile)
-		if err != nil {
-			t.Fatal(err)
-		}
-
-		reqWithFile.Header.Add("Content-Type", mwWithFile.FormDataContentType())
-
-		c.Request = reqWithFile
-
-		h := &imageHandler{}
-		h.ProcessImage(c)
-
-		result := w.Result()
-		assert.Equal(t, result.StatusCode, http.StatusCreated)
-	})
-
-	t.Run("error resize request not valid", func(t *testing.T) {
-		// generate body
+	generateBodyForTest := func(fileImagePath string, resizeHeight, resizeWidth int) *http.Request {
 		body := new(bytes.Buffer)
 		mw := multipart.NewWriter(body)
 
-		resizeHeight, _ := mw.CreateFormField("resizeHeight")
-		resizeHeight.Write([]byte("-50"))
+		if fileImagePath != "" {
+			file, err := os.Open(fileImagePath)
+			if err != nil {
+				t.Fatal(err)
+			}
 
-		resizeWidth, _ := mw.CreateFormField("resizeWidth")
-		resizeWidth.Write([]byte("50"))
+			ws, err := mw.CreateFormFile("file", "flower.png")
+			if err != nil {
+				t.Fatal(err)
+			}
 
-		file, err := os.Open("./image/flower.png")
-		if err != nil {
-			t.Fatal(err)
+			if _, err := io.Copy(ws, file); err != nil {
+				t.Fatal(err)
+			}
 		}
 
-		ws, err := mw.CreateFormFile("file", "flower.png")
-		if err != nil {
-			t.Fatal(err)
-		}
+		resizeH, _ := mw.CreateFormField("resizeHeight")
+		resizeH.Write([]byte(fmt.Sprintf("%d", resizeHeight)))
 
-		if _, err := io.Copy(ws, file); err != nil {
-			t.Fatal(err)
-		}
+		resizeW, _ := mw.CreateFormField("resizeWidth")
+		resizeW.Write([]byte(fmt.Sprintf("%d", resizeWidth)))
 
 		mw.Close()
-		// end generate body
 
-		w := httptest.NewRecorder()
-		c, _ := gin.CreateTestContext(w)
-		reqWithFile, err := http.NewRequest(http.MethodPost, "/", body)
+		req, err := http.NewRequest(http.MethodPost, "/", body)
 		if err != nil {
 			t.Fatal(err)
 		}
 
-		reqWithFile.Header.Add("Content-Type", mw.FormDataContentType())
+		req.Header.Add("Content-Type", mw.FormDataContentType())
 
-		c.Request = reqWithFile
+		return req
+	}
 
-		h := &imageHandler{}
-		h.ProcessImage(c)
+	type field struct {
+		fileImagePath string
+		resizeHeight  int
+		resizeWidth   int
+	}
+	var tests = []struct {
+		name           string
+		field          field
+		wantStatusCode int
+	}{
+		{
+			name:           "success process image",
+			field:          field{"./image/flower.png", 50, 50},
+			wantStatusCode: http.StatusCreated,
+		},
+		{
+			name:           "error resize request not valid",
+			field:          field{"./image/flower.png", -50, 50},
+			wantStatusCode: http.StatusBadRequest,
+		},
+		{
+			name:           "error image request not provided",
+			field:          field{"", 50, 50},
+			wantStatusCode: http.StatusBadRequest,
+		},
+		{
+			name:           "error image is not png",
+			field:          field{"./image/cat.jpg", -50, 50},
+			wantStatusCode: http.StatusBadRequest,
+		},
+	}
 
-		result := w.Result()
-		assert.Equal(t, result.StatusCode, http.StatusBadRequest)
-	})
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			req := generateBodyForTest(
+				tt.field.fileImagePath,
+				tt.field.resizeHeight,
+				tt.field.resizeWidth,
+			)
 
-	t.Run("error image request not provided", func(t *testing.T) {
-		// generate body
-		body := new(bytes.Buffer)
-		mw := multipart.NewWriter(body)
+			w := httptest.NewRecorder()
+			c, _ := gin.CreateTestContext(w)
+			c.Request = req
 
-		mw.Close()
-		// end generate body
+			h := &imageHandler{}
+			h.ProcessImage(c)
 
-		w := httptest.NewRecorder()
-		c, _ := gin.CreateTestContext(w)
-		reqWithFile, err := http.NewRequest(http.MethodPost, "/", body)
-		if err != nil {
-			t.Fatal(err)
-		}
-
-		reqWithFile.Header.Add("Content-Type", mw.FormDataContentType())
-
-		c.Request = reqWithFile
-
-		h := &imageHandler{}
-		h.ProcessImage(c)
-
-		result := w.Result()
-		assert.Equal(t, result.StatusCode, http.StatusBadRequest)
-	})
-
-	t.Run("error image is not png", func(t *testing.T) {
-		// generate body
-		body := new(bytes.Buffer)
-		mw := multipart.NewWriter(body)
-
-		resizeHeight, _ := mw.CreateFormField("resizeHeight")
-		resizeHeight.Write([]byte("50"))
-
-		resizeWidth, _ := mw.CreateFormField("resizeWidth")
-		resizeWidth.Write([]byte("50"))
-
-		file, err := os.Open("./image/cat.jpg")
-		if err != nil {
-			t.Fatal(err)
-		}
-
-		ws, err := mw.CreateFormFile("file", "flower.png")
-		if err != nil {
-			t.Fatal(err)
-		}
-
-		if _, err := io.Copy(ws, file); err != nil {
-			t.Fatal(err)
-		}
-
-		mw.Close()
-		// end generate body
-
-		w := httptest.NewRecorder()
-		c, _ := gin.CreateTestContext(w)
-		reqWithFile, err := http.NewRequest(http.MethodPost, "/", body)
-		if err != nil {
-			t.Fatal(err)
-		}
-
-		reqWithFile.Header.Add("Content-Type", mw.FormDataContentType())
-
-		c.Request = reqWithFile
-
-		h := &imageHandler{}
-		h.ProcessImage(c)
-
-		result := w.Result()
-		assert.Equal(t, result.StatusCode, http.StatusBadRequest)
-	})
-
+			result := w.Result()
+			assert.Equal(t, result.StatusCode, tt.wantStatusCode)
+		})
+	}
 }
